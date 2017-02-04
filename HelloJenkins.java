d90c45033bcc29448d4a32d0ed7531f2f8135060
Sean <seanyoughal92@gmail.com>
2017-02-04 23:18:52 +0000

    testing

---
+++ HelloJenkins.java
@@ -0,0 +1,11 @@
+package com.sean.hellojenkins;
+
+/**
+ * Created by Sean on 04/02/2017.
+ */
+public class HelloJenkins {
+    public static void main(String[] args){
+        //prints Hello World
+        System.out.println("Hello World");
+    }
+}
